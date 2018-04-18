9006225dcbea35e186a5c3dd05d6a39baaf19a9b
Ahmed Samir <asamir97@hotmail.com>
2018-04-18 22:35:08 +0200

    Added IBubble process

--- WB.vhdl
+++ WB.vhdl
@@ -2,14 +2,36 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY WB IS
-	PORT( 
-		RAM_VALUE_OR_DST_RESULT, SRC_RESULT: IN std_logic_vector(15 DOWNTO 0);
-		Rdst_INDEX, Rsrc_INDEX: IN std_logic_vector(2 DOWNTO 0);
+	PORT(
+
+		------------------- CONTROL SIGNALS ----------------------
+		IBUBBLE: IN std_logic;
 		WB_SIGNALS: IN std_logic_vector(2 DOWNTO 0);
+		----------------------------------------------------------
+
+		------------------- INPUT DATA ---------------------------
+		RAM_VALUE_or_DST_RESULT: IN std_logic_vector(15 DOWNTO 0);
+		SRC_RESULT: IN std_logic_vector(15 DOWNTO 0);
+		----------------------------------------------------------
+
+		---------------- REGISTERS SELECTION ---------------------
+		Rdst_INDEX: IN std_logic_vector(2 DOWNTO 0);
+		Rsrc_INDEX: IN std_logic_vector(2 DOWNTO 0);
+		----------------------------------------------------------
+
+
+
+
+
+		----------------- REGISTERS ENABLES ----------------------
 		PC_WRITE_EN: OUT std_logic;
 		REGISTERS_WRITE_ENABLES: OUT std_logic_vector(5 downto 0);
+		----------------------------------------------------------
+
+		------------------- OUTPUT DATA --------------------------
 		PC_VALUE: OUT std_logic_vector(15 DOWNTO 0);
-		OUT_BUS : INOUT std_logic_vector(31 DOWNTO 0)
+		OUT_BUS: INOUT std_logic_vector(31 DOWNTO 0)
+		----------------------------------------------------------
 	);
 END WB;
 
@@ -18,6 +40,19 @@ BEGIN
 	
 	-- Decoder to generate register enable signals from Rsrc and Rdst indexes
 	REGISTERS_DECODER: ENTITY work.DECODER PORT MAP (Rdst_INDEX, Rsrc_INDEX, REGISTERS_WRITE_ENABLES);
+
+	PROCESS(IBUBBLE)
+	BEGIN
+
+		-- INT -> CHANGE PC WITH ISR address
+		IF(IBUBBLE = '1') THEN
+			PC_WRITE_EN <= '1';
+			PC_VALUE <= RAM_VALUE_OR_DST_RESULT;
+		ELSE 
+			PC_WRITE_EN <= '0';
+		END IF;
+
+	END PROCESS;
 
 	PROCESS(WB_SIGNALS)
 	BEGIN
@@ -51,4 +86,5 @@ BEGIN
 		END IF;
 
 	END PROCESS;
+	
 END WB_ARCH;
