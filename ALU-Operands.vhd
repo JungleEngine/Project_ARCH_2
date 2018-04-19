d8a9042912c24ab8f757341f7ff2b200ff10895c
3bhady <mohamed_abdelhady96@hotmail.com>
2018-04-19 04:50:45 +0200

    finished ALU operands selection module

--- ALU-Operands.vhd
+++ ALU-Operands.vhd
@@ -9,22 +9,19 @@ USE IEEE.numeric_std.all;
 
 ENTITY alu_operands IS
  PORT (	
- 		clk : IN std_logic;
-        we : IN std_logic;
-
-		-------------------------< Possible ALU operands--------------------------------
+ 		-------------------------< Possible ALU operands--------------------------------
 		---------Previous ALU results saved in the buffer before Memory Stage---
 		result_dst,result_src,
 		---------Current value of r_src,r_dst from register file----------------
-		r_dst,r_src
-		---------Immediate value forwarded from Fetch stage---------------------
-		immediate
+		register_dst,register_src,
+		---------Immediate value forwarded from Fetch stage when immediate opcode
+		immediate,
 		---------Value forwarded from memory stage if the operand is in memory--
 		memory_forwarded	
 
-		: IN std_logic_vector(15 DOWNTO 0)
-		------------------------- Possible ALU operands />------------------------------
-							
+		: IN std_logic_vector(15 DOWNTO 0);
+		------------------------- Possible ALU operands />-------------------------------
+		
 		-------------------------< Selectors for operands--------------------------------
 		dst_in_result_src,dst_in_result_dst,	--this can be reduced into two selectors only
 		src_in_result_src,src_in_result_dst,	
@@ -32,21 +29,41 @@ ENTITY alu_operands IS
 		src_in_mem,		--pop or load the src.	TODO:find if this is for previous command only or more than one level
 		dst_in_mem,		--pop or load the dst
 
-		dst_is_immediate,	--destination is an immediate value which is passed from fetch
+		dst_is_immediate	--destination is an immediate value which is passed from fetch
 						--TODO: differentiate between forwarded immediate value and the 5 bit shift immediate value
-		: IN std_logic
+						--TODO: on ALU accept shift as input and select it with the opcode
+		: IN std_logic;
 		------------------------- Selectors for operands/>-------------------------------
 		
 		-------------------------< Operands out -----------------------------------------
-		destination,src
+		destination,source
 		: OUT std_logic_vector(15 downto 0)
 		------------------------- Operands out/>-----------------------------------------
-
 						 );
 END ENTITY alu_operands;
 
 ARCHITECTURE alu_operands_arch OF alu_operands IS  
-BEGIN
 
+BEGIN
+	source <= 
+		memory_forwarded	when (src_in_mem='1')
+	else
+		result_src 			when (src_in_result_src='1')
+	else
+		result_dst 			when (src_in_result_dst='1')
+	else
+		register_src;
+
+	destination <=
+		memory_forwarded	when (dst_in_mem='1')
+	else
+		immediate 			when (dst_is_immediate='1')
+	else
+		result_src 			when(dst_in_result_src='1')
+	else
+		result_dst 			when(dst_in_result_dst='1')
+	else
+		register_dst;
+		
 END alu_operands_arch;
 
