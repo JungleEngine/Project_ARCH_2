b65eccb22d9cd316c2be876ad6963c8ab4e4f276
mohamed ali <muhamedali166@gmail.com>
2018-04-21 15:59:04 +0200

    Check added for stall, if stall do nothing.

--- Branch_Detector.vhd
+++ Branch_Detector.vhd
@@ -7,6 +7,7 @@ USE IEEE.STD_LOGIC_SIGNED.all;
 -- Z-N-C
 ENTITY INSTRUCTION_DECODER IS
  PORT (	BRANCH_DETECTED 		: in std_logic;
+ 		DH_STALL 				: in std_logic;
 		FLAGS_REG_OUTPUT 		: in std_logic_vector(3 downto 0);
  		OPCODE                  : in std_logic_vector(4 downto 0);
  		RDST_VALUE				: in std_logic_vector(15 downto 0);				
@@ -30,36 +31,39 @@ ARCHITECTURE ARCH OF INSTRUCTION_DECODER IS
   -- Input to fetch stage.
   DECODING_STAGE_OUTPUT <= RDST_VALUE(8 downto 0);
   -- Z-N-C
-  PROCESS(FLAGS_REG_OUTPUT, BRANCH_DETECTED, RDST_VALUE)
+  PROCESS(BRANCH_DETECTED, DH_STALL, FLAGS_REG_OUTPUT, OPCODE, RDST_VALUE)
   BEGIN 
-  
-  	--If branch is detected.
-  	IF BRANCH_DETECTED = '1' THEN
-  		
-  		-- Check if JMP
-  		iF OPCODE = CONST_OPCODE_JMP THEN
-  			BRANCH_TAKEN <= '1';
-  			FLAGS_REG_INPUT <=	FLAGS_REG_OUTPUT;
-	  	
-	  	-- Check zero flag = 1.
-	  	ELSIF OPCODE = CONST_OPCODE_JZ and FLAGS_REG_OUTPUT(3) = '1' THEN
-	  		BRANCH_TAKEN <= '1';
-	  		FLAGS_REG_INPUT <= ('0' & FLAGS_REG_OUTPUT(2 downto 0));
+  	
+  	-- IF no stall.
+  	IF DH_STALL = '0' THEN
+	  	--If branch is detected.
+	  	IF BRANCH_DETECTED = '1' THEN
+	  		
+	  		-- Check if JMP
+	  		iF OPCODE = CONST_OPCODE_JMP THEN
+	  			BRANCH_TAKEN <= '1';
+	  			FLAGS_REG_INPUT <=	FLAGS_REG_OUTPUT;
+		  	
+		  	-- Check zero flag = 1.
+		  	ELSIF OPCODE = CONST_OPCODE_JZ and FLAGS_REG_OUTPUT(3) = '1' THEN
+		  		BRANCH_TAKEN <= '1';
+		  		FLAGS_REG_INPUT <= ('0' & FLAGS_REG_OUTPUT(2 downto 0));
 
-  		-- Check negative flag = 1.
-  		ELSIF OPCODE = CONST_OPCODE_JN and FLAGS_REG_OUTPUT(2) = '1' THEN
-  			BRANCH_TAKEN <= '1';
-  			FLAGS_REG_INPUT <= (FLAGS_REG_OUTPUT(3) & FLAGS_REG_OUTPUT(2) & '0' & FLAGS_REG_OUTPUT(0));
+	  		-- Check negative flag = 1.
+	  		ELSIF OPCODE = CONST_OPCODE_JN and FLAGS_REG_OUTPUT(2) = '1' THEN
+	  			BRANCH_TAKEN <= '1';
+	  			FLAGS_REG_INPUT <= (FLAGS_REG_OUTPUT(3) & FLAGS_REG_OUTPUT(2) & '0' & FLAGS_REG_OUTPUT(0));
 
-		-- Check carry flag = 1.
-		ELSIF OPCODE = CONST_OPCODE_JC and FLAGS_REG_OUTPUT(1) = '1' THEN
-			BRANCH_TAKEN <= '1';
-			FLAGS_REG_INPUT <= (FLAGS_REG_OUTPUT(3) & '0' & FLAGS_REG_OUTPUT(1 downto 0));
-		ELSE 
-			BRANCH_TAKEN <='0';
-			FLAGS_REG_INPUT <= FLAGS_REG_OUTPUT;
-	  	END IF;
-  	END IF; 
+			-- Check carry flag = 1.
+			ELSIF OPCODE = CONST_OPCODE_JC and FLAGS_REG_OUTPUT(1) = '1' THEN
+				BRANCH_TAKEN <= '1';
+				FLAGS_REG_INPUT <= (FLAGS_REG_OUTPUT(3) & '0' & FLAGS_REG_OUTPUT(1 downto 0));
+			ELSE 
+				BRANCH_TAKEN <='0';
+				FLAGS_REG_INPUT <= FLAGS_REG_OUTPUT;
+		  	END IF;
+	  	END IF; 
+  	END IF;
   END PROCESS;
 END ARCH;
 
